ff1e0e4c93d3bf3029ebebde7d6c70e1bc4bbc98
Sergey Yarmoshuk <sergey.yarmoshuk@gmail.com>
2019-04-26 16:13:55 +0300

    add contorller

--- src/System/App.php
+++ src/System/App.php
@@ -2,6 +2,21 @@
 
 namespace App\System;
 
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Router;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpKernel\Controller;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
+ use Symfony\Component\Routing\Matcher\UrlMatcher;
+
 
 class App
 {
@@ -14,11 +29,14 @@ class App
     private $basePath;
     private static $instance = null;
 
-    private function __construct($basePath)
+    public function getRequest()
     {
-        $this->setRequest();
-        $this->setRequestContext();
-        $this->setTouter();
+        return $this->request;
+    }
+
+    public function getRequestContext()
+    {
+        return $this->requestContext;
     }
 
     public static function getInstance($basePath)
@@ -32,6 +50,56 @@ class App
 
     public function run()
     {
-        echo 'hello world';
+        $matcher = new UrlMatcher($this->routes, $this->requestContext);
+
+        try {
+            $this->request->attributes->add($matcher->match($this->request->getPathInfo()));
+            $controller = $this->getController();
+            dd($controller);
+            //$arguments = $this->getArguments($controller);
+        } catch(\Exeption $e) {
+            die('error');
+        }
+    }
+
+    private function getController()
+    {
+        return (new ControllerResolver())->getController($this->request);
+    }
+
+    private function getArguments()
+    {
+        
+    }
+
+    private function __construct($basePath)
+    {
+        $this->basePath = $basePath;
+        $this->setRequest();
+        $this->setRequestContext();
+        $this->setRouter();
+        $this->routes = $this->router->getRouteCollection();
+    }
+
+    private function setRequest()
+    {
+        $this->request = Request::createFromGlobals();
+    }
+
+    private function setRequestContext()
+    {
+        $this->requestContext = new RequestContext();
+        $this->requestContext->fromRequest($this->request);
+    }
+
+    private function setRouter()
+    {
+        $fileLocator = new FileLocator([__DIR__]);
+
+        $this->router = new Router(
+            new YamlFileLoader($fileLocator),
+            $this->basePath.'/config/routes.yaml',
+            ['cache_dir' => $this->basePath.'storage/cache']
+        );
     }
 }
